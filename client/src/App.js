296b36de8439a51dba102f34274e9bd438fd37d3
sanikabaviskar10 <sanikabaviskar10@gmail.com>
2023-11-06 14:58:44 +0530

    navbar design

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,24 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
-import List from "./pages/list/List";
 import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
 
 
-function App()
-{
+function App() {
   return (
     <BrowserRouter>
-    <Routes>
+      <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/hotels" element={<List/>}/>
         <Route path="/hotels/:id" element={<Hotel/>}/>
-    </Routes>
+       
+      </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
---
+++ client/src/pages/hotel/Hotel.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Hotel =() => {
+    return (
+        <div>Hotel</div>
+    )
+}
+export default Hotel
