4f87ac44007a11875b3f506f87592d6ff83d86ce
sanikabaviskar10 <sanikabaviskar10@gmail.com>
2023-11-06 16:16:50 +0530

    header design

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
 import "./home.css"
 
 const Home =() => {
     return (
         <div>
            <Navbar />
+           <Header />
+         
+          
         </div>
     )
 }
