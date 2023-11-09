d6a986db252030f3731609e6bc94bdb4819deac8
sanikabaviskar10 <sanikabaviskar10@gmail.com>
2023-11-09 16:18:45 +0530

    sidebar design

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss"
 
 const Home = () => {
     return (
         <div className="home">
-           home
+           <Sidebar/>
+           <div className="homeContainer">container</div>
         </div>
     )
 }
