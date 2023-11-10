9721a49ca263a837d85b46c7cba1eaec48453208
sanket gaikwad <gaikwad30sanket@gmail.com>
2023-11-10 11:49:03 +0530

    Login Auth Done

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -15,6 +15,7 @@ import "react-date-range/dist/theme/default.css"; // theme css file
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
 import { SearchContext } from "../../context/SearchContext";
+import { AuthContext } from "../../context/AuthContext";
 
 const Header = ({ type }) => {
   const [destination, setDestination] = useState("");
@@ -43,6 +44,9 @@ const Header = ({ type }) => {
       };
     });
   };
+
+  const{user} = useContext(AuthContext)
+
 
   const {dispatch} = useContext(SearchContext)
 
@@ -89,7 +93,7 @@ const Header = ({ type }) => {
               Get rewarded for your travels – unlock instant savings of 10% or
               more with a free Lamabooking account
             </p>
-            <button className="headerBtn">Sign in / Register</button>
+            {!user && <button className="headerBtn">Sign in / Register</button>}
             <div className="headerSearch">
               <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faBed} className="headerIcon" />
--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { SearchContextProvider } from './context/SearchContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <SearchContextProvider>
-     <App />
-  </SearchContextProvider>
+  <AuthContextProvider>
+    <SearchContextProvider>
+      <App />
+    </SearchContextProvider>
+  </AuthContextProvider>
   </React.StrictMode>
 );
 
