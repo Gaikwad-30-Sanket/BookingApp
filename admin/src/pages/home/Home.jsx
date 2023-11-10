812c56461939fc3ae3e0e942d2a725b95fee04d8
sanikabaviskar10 <sanikabaviskar10@gmail.com>
2023-11-10 16:13:46 +0530

    progressbar component

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -2,8 +2,8 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
 import Widget from "../../components/widget/Widget";
-//import Featured from "../../components/featured/Featured";
-//import Chart from "../../components/chart/Chart";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
 //import Table from "../../components/table/Table";
 
 const Home = () => {
@@ -17,6 +17,10 @@ const Home = () => {
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
         </div>
        
         </div>
