0af47e08cbf8d73cf51a5841851fcc903366b933
sanikabaviskar10 <sanikabaviskar10@gmail.com>
2023-11-23 15:17:02 +0530

    error clean

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { DarkModeContextProvider } from "./context/darkModeContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
+import { DarkModeContextProvider } from './context/darkModeContext';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <DarkModeContextProvider>
+  <AuthContextProvider>
+     <DarkModeContextProvider> 
       <App />
-    </DarkModeContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+     </DarkModeContextProvider>
+  </AuthContextProvider>
+  </React.StrictMode>
 );
-
